d25cec310b3033efbd3bd2b7ae77daf881e61195
barrymillham <j.mart.x93@gmail.com>
2014-04-23 22:46:54 -0400

    Hunky
    Bunky

--- HtmlTree/src/htmltree/HtmlTree.java
+++ HtmlTree/src/htmltree/HtmlTree.java
@@ -13,6 +13,7 @@ import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileWriter;
